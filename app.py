0ce51ba48b0794dbb05fb79c8c438d989d651ae8
sskarthigadevi <devisskarthigaa@gmail.com>
2025-02-01 15:09:38 +0530

    default values assigned
    default values assigned to length and number

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import random
 
-numbers = input('How many passwords needed: ')
+numbers = 5 #input('How many passwords needed: ')
 letters = '1234567890-=!@#$%^&*()_+asdfghjkl:"zxcvbnm<>?ASDFGHJKL;ZXCVBNM,./QWERTYUIOP[]{}\''
-length = input('What is the length of password: ')
+length = 9 #input('What is the length of password: ')
 
 for x in range(int(numbers)):
     pwd = ''
+    print("THIS IS THE RANDOM PASSWORD GENERATOR FOR 5 NUMBERS WITH LENGTH=9")
     for _ in range(int(length)):
         pwd += random.choice(letters)
     print(pwd)
